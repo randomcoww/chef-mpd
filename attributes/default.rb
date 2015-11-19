d0cb4658f0f248f4f596ddfa764c367a80440a55
randomcoww <randomcoww@dev8.dyn.local>
2015-11-18 21:58:55 -0800

    Added missing chef attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,1 @@
 node.override['chef_client']["daemon_options"] = ["-z", "-c", "/etc/chef/solo.rb", "-j", "/etc/chef/node.json"]
-node.override['chef_client']["init_style"] = "runit"
-node.override['chef_client']["interval"] = 60
-node.override['chef_client']["splay"] = 0
